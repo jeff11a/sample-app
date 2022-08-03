0aaac51522e511cd051b4e3dcc8b14c2ff9a3ee2
jeffos1 <villa4ks@gmail.com>
2022-08-03 23:06:27 +0000

    port 5050

--- sample-app.sh
+++ sample-app.sh
@@ -20,3 +20,4 @@ cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a 
+
--- sample_app.py
+++ sample_app.py
@@ -11,3 +11,4 @@ def main():
 
 if __name__ == "__main__":
     sample.run(host="0.0.0.0", port=5050)
+    
